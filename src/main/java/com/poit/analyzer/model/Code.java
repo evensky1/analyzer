340bf13a89412b5c2ded58f7432aea6b71b31a59
evenski <fromthedeepsize@gmail.com>
2021-09-12 15:59:08 +0300

    upd

---
+++ src/main/java/com/poit/analyzer/model/Code.java
@@ -0,0 +1,14 @@
+package com.poit.analyzer.model;
+
+public class Code {
+    //Тут можно организовать логику самого парсера
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
