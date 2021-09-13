ae394b530f4e3e8adecacf3eebd9d91903496482
evenski <fromthedeepsize@gmail.com>
2021-09-13 20:43:34 +0300

    commit

--- src/main/java/com/poit/analyzer/controllers/PageController.java
+++ src/main/java/com/poit/analyzer/controllers/PageController.java
@@ -1,17 +1,25 @@
 package com.poit.analyzer.controllers;
 
+import com.poit.analyzer.model.Code;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PageController {
     @GetMapping
-    public String page() {
+    public String page(Model model) {
+        model.addAttribute("inputCode", new Code());
         return "mainPage";
     }
-
-    @GetMapping("/start")
-    public String testCall() {
-        return "mainPage2";
+    @PostMapping
+    public String page(@ModelAttribute("inputCode") Code code, Model model){
+        //строка передаётся в класс-модель Code, в котором следует организовать логику парсера
+        System.out.println(code.getCode()); //тестирование
+        //пока не разобрался как отставить код программы в textarea после отправки
+        return "mainPage";
     }
 }
--- src/main/java/com/poit/analyzer/model/Code.java
+++ src/main/java/com/poit/analyzer/model/Code.java
@@ -1,9 +1,15 @@
 package com.poit.analyzer.model;
 
+import java.util.HashMap;
+
 public class Code {
     //Тут можно организовать логику самого парсера
     private String code;
-
+    //Была идея использовать хэшмап, но из-за непостояноого кол-ва операндов, пока забил
+    public Code(){}
+    public Code(String code){
+        this.code = code;
+    }
     public String getCode() {
         return code;
     }
