c8098713badbb9aa342d5261dca8e24a32540bdf
evenski <fromthedeepsize@gmail.com>
2021-10-05 18:59:42 +0300

    Микро-правки и добавление строк как литералов

--- src/main/java/com/poit/analyzer/model/Code.java
+++ src/main/java/com/poit/analyzer/model/Code.java
@@ -133,31 +133,26 @@ public class Code {
 
     public HashMap<String, Integer> codeOperandAnalyzing() {
         resultOperandsTable = new HashMap<>();
-        String codeTemp = deleteCommentsAndStringsFromCode(code);
-        //поиск инициализаций переменных и внесение их(переменных) в мапу
-        Pattern pattern = Pattern.compile("\\b[_a-zA-Z][_\\da-zA-Z]*\\b(?=\\s*=)");
+        String codeTemp = code.replaceAll("(=begin\\s(.*\\r?\\n)*?=end\\s)|(#.*)", "");
+        Pattern pattern = Pattern.compile("\".*?[^\\\\](\\\\\\\\)*\"");
         Matcher matcher = pattern.matcher(codeTemp);
-        while (matcher.find()) {
-            resultOperandsTable.putIfAbsent(matcher.group(), 0);
-        }
-        //подсчёт вхождений каждой переменной
-        for (String varOperand : resultOperandsTable.keySet()) {
-            int count = 0; //так не хотелось много раз вызывать put и get
-            pattern = Pattern.compile("\\b" + varOperand + "\\b");
-            matcher = pattern.matcher(codeTemp);
-            while (matcher.find()) {
-                count++;
+        while(matcher.find()){
+            if (resultOperandsTable.get(matcher.group()) == null) {
+                resultOperandsTable.put(matcher.group(), 1);
+            } else {
+                resultOperandsTable.put(matcher.group(), resultOperandsTable.get(matcher.group()) + 1);
             }
-            resultOperandsTable.put(varOperand, count);
         }
-        //подсчёт литералов
-        String[] patterns = {"(?<!\\.)(?<=\\W)\\d+", "(?<=\\W)\\d+(?=(\\.\\d+))", "true|false", "\".\""};
-        for (String regExp : patterns) {
+        codeTemp = codeTemp.replaceAll("\".*?[^\\\\](\\\\\\\\)*\"", "");
+        //поиск инициализаций переменных и внесение их(переменных) в мапу
+        String[] patterns = {"\\b[_a-zA-Z][_\\da-zA-Z]*\\b(?=\\s*=)", "(?<!\\.)(?<=\\W)\\d+",
+                                "(?<=\\W)\\d+(?=(\\.\\d+))", "true|false"};
+        for(String regExp: patterns){
             pattern = Pattern.compile(regExp);
             matcher = pattern.matcher(codeTemp);
-            while (matcher.find()) {
+            while(matcher.find()){
                 if (resultOperandsTable.get(matcher.group()) == null) {
-                    resultOperandsTable.put(matcher.group(), 1);
+                        resultOperandsTable.put(matcher.group(), 1);
                 } else {
                     resultOperandsTable.put(matcher.group(), resultOperandsTable.get(matcher.group()) + 1);
                 }
@@ -165,9 +160,7 @@ public class Code {
         }
         return resultOperandsTable;
     }
-
     public String deleteCommentsAndStringsFromCode(String code) {
-        // Очевидно, что то, что в строках и комментариях, считать не стоит
         String codeTemp = code.replaceAll("\".*?[^\\\\](\\\\\\\\)*\"", "");
         codeTemp = codeTemp.replaceAll("(=begin\\s(.*\\r?\\n)*?=end\\s)|(#.*)", "");
         return codeTemp;
